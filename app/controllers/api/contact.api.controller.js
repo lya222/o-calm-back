c65d68a20af37c806e39b97c4dc5fa99fe235386
Live session user <formation_d_9@outlook.fr>
2024-09-02 00:19:04 +0000

    update controleur contact

--- app/controllers/api/contact.api.controller.js
+++ app/controllers/api/contact.api.controller.js
@@ -2,25 +2,20 @@ import CoreController from './core.api.controller.js';
 
 import { contactDataMapper } from '../../datamappers/index.datamapper.js';
 
-//20 aout Etape 4 creation du controller fini maintenant faire route
 export default class ContactApiController extends CoreController {
 
 
     static entityName = "contact";
     static properDatamapper = contactDataMapper;
 
-       /**
-     * Crée une nouvelle entité.
-     * @param {Object} req - Objet de requête.
-     * @param {Object} res - Objet de réponse.
-     * @returns {Promise<Object>} Les données créées.
-     */
        static async createContact(req, res) {
-        const input = req.body;
-        const row = await this.properDatamapper.createContact(input);
-
-        return res.status(201).json({ data: row });
+        const {name, email, message} = req.body;
+         const row = await this.properDatamapper.createContact(name, email, message);
+         return res.status(201).json({data: row});
+      
+      }
     };
 
 
-};
+
+
